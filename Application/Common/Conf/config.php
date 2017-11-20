19d530428323bf485f8630ab713b7efce55c1caa
jinkuang <jinkuanghqu@gmail.com>
2017-11-20 09:14:41 +0800

    delete

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    'DB_TYPE'              => 'mysqli', // 数据库类型
+    'DB_HOST'              => '127.0.0.1', // 服务器地址
+    'DB_NAME'              => 'admin', // 数据库名
+    'DB_USER'              => 'root', // 用户名
+    'DB_PWD'               => 'root', // 密码
+    'DB_PORT'              => '3306', // 端口
+    'DB_PREFIX'            => '', // 数据库表前缀
+
+    'SHOW_PAGE_TRACE'      => false,
+    'URL_CASE_INSENSITIVE' => false,
+
+    'APP_KEY'              => 'fasdf#$%#KJUSDa26s',
+
+    'TMPL_L_DELIM'         => '{{',
+    'TMPL_R_DELIM'         => '}}',
+    'URL_MODEL'            => '2',
+);
